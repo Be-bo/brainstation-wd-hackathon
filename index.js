1ec8edcfdae55f1cdab1b1e8869e7613f0ff2663
daaniyaal <daaniyaalmirza@gmail.com>
2023-10-13 12:16:17 -0700

    Added url variables

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 let apiKey = "24698e1e0dde7266415dbae978800489";
 let appId = "e7e63e83";
+let appUrl = "https://api.edamam.com/api/recipes/";
+let appBaseUrl = apUrl + appId + +apiKey;
+let searchQuest ="https://api.edamam.com/search?q=`${searchqueryinput}`&app_id=e7e63e83&app_key=24698e1e0dde7266415dbae978800489&`${category}`" 
+alert("test");
 
-alert('test');
+const searchFunction () {
+   // input for 
+}
