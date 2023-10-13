2991c2c2bdc41ab0eeb6c3f9be7b1a338b6d2042
Be-bo <robfiker@gmail.com>
2023-10-13 12:23:18 -0700

    Added get reqest axios function

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
+'use strict';
 let apiKey = "24698e1e0dde7266415dbae978800489";
-let appId = "e7e63e83";
+let appId = "e7e63e83"
 
-alert('test');
+
+
+async function getRequest(baseUrl, ...queryArgs){
+    url = baseUrl;
+    for(let i = 0; i<queryArgs.length; i++) url+=queryArgs[i];
+
+    return axios.get(url)
+    .then(response => {
+        return(response.data);
+    })
+    .catch(errur => {
+        console.error(errur.message);
+    });
+}
